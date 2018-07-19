836d6764ae6ef920d245f850e27f05a88a44de34
Austin Barr and Hisato Kawaminami <hisato@>
2018-07-19 16:20:49 -0700

    grab authorId and bookId and insert into join table

--- Library/Controllers/BooksController.cs
+++ Library/Controllers/BooksController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using Library.Models;
+using System;
 
 namespace Libary.Controllers
 {
@@ -28,15 +29,20 @@ namespace Libary.Controllers
     [HttpGet("/author/{id}/add")]
     public ActionResult AddAuthor(int id)
     {
+      Dictionary<string,object> model = new Dictionary<string,object>{};
       Book thisBook = Book.FindBook(id);
       List<Author> allAuthors = Author.GetAllAuthors();
+      model.Add("bookid", id);
+      model.Add("authors", allAuthors);
 
-      return View(allAuthors);
+
+      return View(model);
     }
     [HttpPost("/author/{id}/add")]
     public ActionResult CollectAuthorForBook(int id)
     {
-      Author newAuthor = new Author(Request.Form["newauthor"]);
+      Console.WriteLine(id);
+      Author newAuthor = Author.FindAuthor(int.Parse(Request.Form["newauthor"]));
       Book thisBook = Book.FindBook(id);
       thisBook.AddAuthor(newAuthor);
       return RedirectToAction("Catalog");
